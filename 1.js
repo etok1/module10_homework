e9b1d0384b25fc1b48fd62bc24f3004acc81cbb2
Eva Tokmakova <eva.tokmakova12@gmail.com>
2023-11-20 23:36:34 +0600

    fixed first task

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@ let input = prompt("Please enter something");
 
 let number = +input;
 
-if (number === number) {
+if (number === "number") {
   if (number % 2 === 0) {
     console.log("Число четное");
   } else {
